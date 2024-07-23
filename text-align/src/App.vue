9253a91bdd967ead1025e8c2dca286403b0049f7
Young-Je Park <young-jepark@192.168.nate.com>
2024-07-23 16:01:35 +0900

    0723 test

---
+++ text-align/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+
+  </header>
+<div > </div>
+  <main>
+ 
+    <div class="ddd">
+      gpffh
+    </div>
+    <div class="ccc">
+      gpffh
+    </div>
+
+
+  </main>
+</template>
+
+<style>
+.ddd {
+align-tiems: center;
+border:1px solid red;
+}
+.ccc {
+  text-align: right;
+  border:1px solid white;
+}
+</style>
